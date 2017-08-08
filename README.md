99df6a69e65625b9d60b97f5300b0519e298f578
Dario Dorella <dario.dorella@gmail.com>
2017-08-08 14:23:52 -0700

    Push repository on GitHub

---
+++ README.md
@@ -0,0 +1,8 @@
+This script was delevoped to help researching the behavior of a certain class users in order to better define a design problem. We needed to know how many files per directory the users deal with.
+
+An example run:
+
+$ python ./countEntries.py ~
+Walked through 155486 directories
+Mean number of entries per directory: 5.245553
+Maximum number of entries in a directory: 35609
---
+++ countEntries.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+import sys
+import os
+
+# Get one parameter that is the root of the tree we want to count
+rootDir="/"
+if len(sys.argv) >= 2:
+    rootDir=sys.argv[1]
+
+nDirsWalked=0
+avgItemsPerDir=0.0
+maxEntries=0
+
+# Walk the directory tree and calcuate average and maximum amount of entries oer
+# directory.
+sys.stdout.write(("Walked through %d directories") % (nDirsWalked))
+sys.stdout.flush()
+for thisDir,subDirs,files in os.walk(rootDir):
+    nDirsWalked+=1
+    nSubDirs=len(subDirs)
+    nFiles=len(files)
+    nTot=nSubDirs + nFiles
+    if nTot > maxEntries:
+        maxEntries=nTot
+    # We do not know the total amount of directories upfront, nor we want to
+    # sum the total number of entries as that might overflow, we use an
+    # iterative method to evaluate the mean:
+    # http://www.heikohoffmann.de/htmlthesis/node134.html
+    avgItemsPerDir+=(nTot - avgItemsPerDir) / nDirsWalked
+    sys.stdout.write(("\rWalked through %d directories") % (nDirsWalked))
+    sys.stdout.flush()
+sys.stdout.write("\n")
+sys.stdout.flush()
+print("Mean number of entries per directory: %f") % (avgItemsPerDir)
+print("Maximum number of entries in a directory: %d") % (maxEntries)
