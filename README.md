def08625142f44c4aa57d15f65ed76f94dec9158
Dario Dorella <dario.dorella@gmail.com>
2017-08-09 10:27:54 -0700

    Handle CTRL-C by printing partial results and exiting.

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ Walked through 155563 directories
 Mean number of entries per directory: 5.248497
 Maximum number of entries in a directory: 35622 (/Users/dario/Library/Caches/Metadata/Safari/History)
 ```
+
+When during the run CTRL-C is pressed, the stats up to that point are printed:
+```text
+$ python ./countEntries.py ~
+Walked through 1942 directories^C
+Mean number of entries per directory: 9.330072
+Maximum number of entries in a directory: 776 (/Users/dario/Documents/Projects/test)
+```
--- countEntries.py
+++ countEntries.py
@@ -1,8 +1,31 @@
 #!/usr/bin/python
 import sys
 import os
+import signal
 
-# Get one parameter that is the root of the tree we want to count
+'''
+As walking the filesystem might take time, when user press CTRL-C (SIGINT) print
+out partial results.
+'''
+def handlerSIGINT(signal, frame):
+        finalOutput()
+        sys.exit(0)
+
+signal.signal(signal.SIGINT, handlerSIGINT)
+
+'''
+This is to avoid writing the code twice, this function is used to write out the
+sumamry at the end of the run or when we catch a CTRL-C (SIGINT)
+'''
+def finalOutput():
+    sys.stdout.write("\n")
+    sys.stdout.flush()
+    print("Mean number of entries per directory: %f") % (avgItemsPerDir)
+    print("Maximum number of entries in a directory: %d (%s)") % (maxEntries[1],maxEntries[0])
+
+'''
+Get one parameter that is the root of the tree we want to count
+'''
 rootDir="/"
 if len(sys.argv) >= 2:
     rootDir=sys.argv[1]
@@ -11,8 +34,10 @@ nDirsWalked=0
 avgItemsPerDir=0.0
 maxEntries=('',0)
 
-# Walk the directory tree and calcuate average and maximum amount of entries oer
-# directory.
+'''
+Walk the directory tree and calcuate average (mean) and maximum amount of
+entries per directory.
+'''
 sys.stdout.write(("Walked through %d directories") % (nDirsWalked))
 sys.stdout.flush()
 for thisDir,subDirs,files in os.walk(rootDir):
@@ -22,14 +47,13 @@ for thisDir,subDirs,files in os.walk(rootDir):
     nTot=nSubDirs + nFiles
     if nTot > maxEntries[1]:
         maxEntries=(thisDir,nTot)
-    # We do not know the total amount of directories upfront, nor we want to
-    # sum the total number of entries as that might overflow, we use an
-    # iterative method to evaluate the mean:
-    # http://www.heikohoffmann.de/htmlthesis/node134.html
+    '''
+    We do not know the total amount of directories upfront, nor we want to
+    sum the total number of entries as that might overflow, we use an
+    iterative method to evaluate the mean:
+    http://www.heikohoffmann.de/htmlthesis/node134.html
+    '''
     avgItemsPerDir+=(nTot - avgItemsPerDir) / nDirsWalked
     sys.stdout.write(("\rWalked through %d directories") % (nDirsWalked))
     sys.stdout.flush()
-sys.stdout.write("\n")
-sys.stdout.flush()
-print("Mean number of entries per directory: %f") % (avgItemsPerDir)
-print("Maximum number of entries in a directory: %d (%s)") % (maxEntries[1],maxEntries[0])
+finalOutput()
