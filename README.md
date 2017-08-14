3f6525eb4047e4dc61dfee45c83cd404fb5d61b3
Dario Dorella <dario.dorella@gmail.com>
2017-08-14 13:14:01 -0700

    Refactor code Add histogram holder for future use (e.g. plotting the actual histogram/curve) Add standard deviation computing

--- README.md
+++ README.md
@@ -14,30 +14,33 @@ optional arguments:
 
 An example run:
 ```text
-$ python ./countEntries.py ~
-Walked through 155563 directories
-Mean number of entries per directory: 5.248497
-Maximum number of entries in a directory: 35622 (/Users/dario/Library/Caches/Metadata/Safari/History)
+$ python ./countEntries.py /Users/dario/Downloads/
+Walked through 28 directories
+Maximum number of entries in a directory: 53 (/Users/dario/Downloads/)
+Mean number of entries per directory: 6.107143
 ```
 
 When during the run CTRL-C is pressed, the stats up to that point are printed:
 ```text
-$ python ./countEntries.py ~
-Walked through 1942 directories^C
-Mean number of entries per directory: 9.330072
-Maximum number of entries in a directory: 776 (/Users/dario/Documents/Projects/test)
+$ python ./countEntries.py ~/Documents/Projects/
+Walked through 907 directories^C
+Maximum number of entries in a directory: 299 (/Users/dario/Documents/Projects/mesa/src/compiler/glsl/glcpp/tests)
+Mean number of entries per directory: 10.650496
+Standard deviation for the number of entries per directory: 12.187871
 ```
 
 You can specify a directory name to skip (this feature needs improvement): a directory matching exactly the name given (no wildcards yet) will be skipped over:
 ```text
 $ python ./countEntries.py --skip lib --skip tmp /var
 Walked through 11285 directories
-Mean number of entries per directory: 3.750908
 Maximum number of entries in a directory: 2476 (/var/db/receipts)
+Mean number of entries per directory: 3.750908
+Standard deviation for the number of entries per directory: 12.187871
 ```
 ```text
 $ python ./countEntries.py --skip tmp --skip db /var
 Walked through 11483 directories
-Mean number of entries per directory: 3.494383
 Maximum number of entries in a directory: 1225 (/var/folders/3f/shsh51yd4ps6ml8nspl7fyj40000gn/C/com.apple.IconServices)
+Mean number of entries per directory: 3.494383
+Standard deviation for the number of entries per directory: 12.187871
 ```
--- countEntries.py
+++ countEntries.py
@@ -3,6 +3,7 @@ import sys
 import os
 import signal
 import argparse
+from math import sqrt
 
 
 '''
@@ -25,15 +26,79 @@ def handlerSIGINT(signal, frame):
 signal.signal(signal.SIGINT, handlerSIGINT)
 
 '''
+The directory class
+'''
+class directorySummary:
+
+    def __init__(self):
+        self.__histogram={}
+        self.__maxEntries=('',0)
+        self.__meanEntries=0.0
+        self.__nDirs=0
+
+    def addDir(self,n,pathname):
+        '''
+        Update the total numebr of entries added
+        '''
+        self.__nDirs+=1
+
+        '''
+        Update the sumamry histogram
+        '''
+        if n in self.__histogram:
+            self.__histogram[n]+=1
+        else:
+            self.__histogram[n]=1
+
+        '''
+        Update maximum numebr of entries per directory
+        '''
+        if n > self.__maxEntries[1]:
+            if pathname != None:
+                self.__maxEntries=(pathname,n)
+            else:
+                self.__maxEntries=('',n)
+
+
+        '''
+        We do not know the total amount of directories upfront, nor we want to
+        sum the total number of entries as that might overflow, we use an
+        iterative method to evaluate the mean:
+        http://www.heikohoffmann.de/htmlthesis/node134.html
+        '''
+        self.__meanEntries+=(n - self.__meanEntries) / self.__nDirs
+
+    def nEntries(self):
+        return self.__nDirs
+
+    def maxEntries(self):
+        return self.__maxEntries
+
+    def meanEntries(self):
+        return self.__meanEntries
+
+    def stdDev(self):
+        dev=0.0
+        if (self.__nDirs > 1):
+            for x in self.__histogram:
+                dev+=(self.__histogram[x] - self.__meanEntries) ** 2
+            dev = sqrt(dev / (self.__nDirs - 1))
+        return dev
+
+    def dump(self):
+        for n in sorted(self.__histogram):
+            print "%d: %d" % (n,self.__histogram[n])
+
+'''
 This is to avoid writing the code twice, this function is used to write out the
 summary at the end of the run or when we catch a CTRL-C (SIGINT)
 '''
 def finalOutput():
     sys.stdout.write("\n")
     sys.stdout.flush()
-    print("Mean number of entries per directory: %f") % (avgItemsPerDir)
-    print("Maximum number of entries in a directory: %d (%s)") % (maxEntries[1],maxEntries[0])
-
+    print("Maximum number of entries in a directory: %d (%s)") % (summary.maxEntries()[1],summary.maxEntries()[0])
+    print("Mean number of entries per directory: %f") % (summary.meanEntries())
+    print("Standard deviation for the number of entries per directory: %f") % (summary.stdDev())
 
 def skipDir(dir):
     if (args.skip != None) and (dir in args.skip):
@@ -41,17 +106,14 @@ def skipDir(dir):
     else:
         return False
 
-
 rootDir=args.pathname
-nDirsWalked=0
-avgItemsPerDir=0.0
-maxEntries=('',0)
+summary=directorySummary()
 
 '''
 Walk the directory tree and calculate average (mean) and maximum amount of
 entries per directory.
 '''
-sys.stdout.write(("Walked through %d directories") % (nDirsWalked))
+sys.stdout.write(("Walked through %d directories") % (summary.nEntries()))
 sys.stdout.flush()
 for thisDir,subDirs,files in os.walk(rootDir,topdown=True):
 
@@ -60,19 +122,14 @@ for thisDir,subDirs,files in os.walk(rootDir,topdown=True):
     '''
     subDirs[:] = [d for d in subDirs if not skipDir(d)]
 
-    nDirsWalked+=1
+    '''
+    Get the number of entries for this directory
+    '''
     nSubDirs=len(subDirs)
     nFiles=len(files)
     nTot=nSubDirs + nFiles
-    if nTot > maxEntries[1]:
-        maxEntries=(thisDir,nTot)
-    '''
-    We do not know the total amount of directories upfront, nor we want to
-    sum the total number of entries as that might overflow, we use an
-    iterative method to evaluate the mean:
-    http://www.heikohoffmann.de/htmlthesis/node134.html
-    '''
-    avgItemsPerDir+=(nTot - avgItemsPerDir) / nDirsWalked
-    sys.stdout.write(("\rWalked through %d directories") % (nDirsWalked))
+    summary.addDir(nTot,thisDir)
+
+    sys.stdout.write(("\rWalked through %d directories") % (summary.nEntries()))
     sys.stdout.flush()
 finalOutput()
